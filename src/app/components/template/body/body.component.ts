9da49e971a26cd6e28f75a53e86fe6540cfa911c
Vinicius <viniciusfrederico1003@gmail.com>
2024-10-29 11:15:54 -0300

    AAAA

--- src/app/components/template/body/body.component.ts
+++ src/app/components/template/body/body.component.ts
@@ -4,17 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ChipsModule } from 'primeng/chips';
+
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [FormsModule, InputTextModule, FloatLabelModule, CardModule, ButtonModule],
+  imports: [FormsModule, InputTextModule, FloatLabelModule, CardModule, ButtonModule, InputMaskModule, InputTextareaModule,ChipsModule],
 
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss'
 })
 export class BodyComponent {
   valor: string = '';
-  valor1: string = '';
+  isbn_value: number = 0;
   value: string = '';
+  values3: string = '';
 
 }
